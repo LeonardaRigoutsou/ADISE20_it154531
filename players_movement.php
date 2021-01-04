a20abe2c24f7dd213e026ec1bf4a1a94cbcf4512
LeonardaRigoutsou <73657361+LeonardaRigoutsou@users.noreply.github.com>
2021-01-04 02:15:16 +0200

    elegxos tou game_status gia started mode

---
+++ players_movement.php
@@ -0,0 +1,85 @@
+<?php
+    $input = json_decode(file_get_contents('php://input'),true);
+    if(isset($_SERVER['HTTP_X_TOKEN'])) {
+        $input['token']=$_SERVER['HTTP_X_TOKEN'];
+    }
+    include_once dirname(__FILE__) . '/dbconnect.php';
+
+    $token = $input['token'];
+    $column = $input['column'];
+
+    $sql1 = 'SELECT status FROM game_status';
+    $stmt1 = $mysqli->prepare($sql1);
+    $stmt1->execute();
+    $res = $stmt1->get_result();
+    $data = $res->fetch_assoc();
+    if ($data['status'] != 'started'){
+        http_response_code(400);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Wrong status! Only in started mode you can play a game !"]);
+        exit;
+    }
+
+    $sql = 'SELECT id FROM players WHERE token = ?';
+    $stmt = $mysqli->prepare($sql);
+    if (!$stmt) {
+        http_response_code(500);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Movement failed1"]);
+        exit;
+    }
+    $stmt->bind_param('s',$token);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    if (!$res){
+        http_response_code(500);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Movement failed2"]);
+        exit;
+    }
+    
+    $data = $res->fetch_assoc();
+    if(!$data){
+        http_response_code(400);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Bad token"]);
+        exit;
+    }
+
+    $sql = 'CALL players_movement(?,?)';
+    $stmt = $mysqli->prepare($sql);
+    if (!$stmt) {
+        http_response_code(500);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Movement failed3"]);
+        exit;
+    }
+    $stmt->bind_param('ii',$column,$data['id']);
+
+    if (!$stmt->execute()) {
+        http_response_code(500);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Movement failed4",'er'=>$stmt->error]);
+        exit;
+    }
+
+    $res1 = $stmt->get_result();
+    $data1 = $res1->fetch_assoc();
+    if ($data1['success'] === 0) {
+        http_response_code(400);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Not your turn"]);
+        exit;
+    }else{
+        http_response_code(400);
+        header('Content-type: application/json');
+        print json_encode(['errormesg'=>"Success"]);
+        exit;
+    }
+
+    header('Content-type: application/json');
+    print json_encode(['errormesg'=>"Success!"]);
+
+
+
+?>
